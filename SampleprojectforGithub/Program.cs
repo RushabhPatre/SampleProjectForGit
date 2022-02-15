d72b65f6d7a6fcd7f423ad6f6e71f3abe9898e6f
Rushabh <patrerushabh2468@gmail.com>
2022-02-15 19:03:27 +0530

    First Commit

---
+++ SampleprojectforGithub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleprojectforGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
